57e9780af7276840be1bc34aa0f0e6814035a685
wayhome25 <siwabada@gmail.com>
2017-08-05 20:24:52 +0900

    FIX: 회원가입시 save 메소드 활용

--- django_app/member/views/auth.py
+++ django_app/member/views/auth.py
@@ -44,7 +44,7 @@ def signup(request):
     if request.method == "POST":
         form = SignupForm(data=request.POST, files=request.FILES)
         if form.is_valid():
-            user = form.create_user()
+            user = form.save()
             django_login(request, user)
             return redirect('member:my_profile')
         else:
