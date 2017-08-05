3ef5a27e30c970fe800b242343b2c2b72c65ec89
bbungsang <821heejin@naver.com>
2017-08-06 00:23:01 +0900

    리뷰 반영

--- django_app/book_store/models/comment.py
+++ django_app/book_store/models/comment.py
@@ -6,12 +6,12 @@ from book_store.models.book import Book, Transaction
 
 
 class BookComment(TimeStampedModel):
-    book_info = models.ForeignKey(Book)
+    book_info = models.ForeignKey(Book, verbose_name='책 정보')
     author = models.ForeignKey(settings.AUTH_USER_MODEL)
     content = models.TextField()
 
 
 class TransactionComment(TimeStampedModel):
-    post = models.ForeignKey(Transaction)
+    post = models.ForeignKey(Transaction, verbose_name='거래 정보')
     author = models.ForeignKey(settings.AUTH_USER_MODEL, verbose_name='댓글 작성자')
     content = models.TextField('댓글 내용')
